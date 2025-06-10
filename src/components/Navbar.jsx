67ae1b0d099203027995bb96c16c42e6138483ef
Manish kumar <manish04kumarsingh@gmail.com>
2025-06-10 20:59:23 +0530

    feat: update ExploreDropdown and NotificationDropdropdown components with animation effects and styling adjustments

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,122 +1,135 @@
-import React, { useState } from "react";
-import { Link } from "react-router";
-import SearchIcon from "../icons/SearchIcon";
-import BellIcon from "../icons/BellIcon";
-import ChatIcon from "../icons/ChatIcon";
-import DropdownIcon from "../icons/DropdownIcon";
-import DiscoverIcon from "../icons/DiscoverIcon";
-import TrendingIcon from "../icons/TrendingIcon";
-import LikeIcon from "../icons/LikeIcon";
-import UserPrefrenceIcon from "../icons/UserPrefrenceIcon";
-import ExploreDropdown from "./ExploreDropdown";
-import NotificationDropdropdown from "./NotificationDropdropdown";
-import { motion } from "framer-motion";
+  import React, { useState } from "react";
+  import { Link } from "react-router";
+  import SearchIcon from "../icons/SearchIcon";
+  import BellIcon from "../icons/BellIcon";
+  import ChatIcon from "../icons/ChatIcon";
+  import DropdownIcon from "../icons/DropdownIcon";
+  import DiscoverIcon from "../icons/DiscoverIcon";
+  import TrendingIcon from "../icons/TrendingIcon";
+  import LikeIcon from "../icons/LikeIcon";
+  import UserPrefrenceIcon from "../icons/UserPrefrenceIcon";
+  import ExploreDropdown from "./ExploreDropdown";
+  import NotificationDropdropdown from "./NotificationDropdropdown";
+  import { motion } from "framer-motion";
 
 
-import UserDropdropdown from "./UserDropdropdown";
-const Navbar = () => {
-  const [ShowExploreDropdown, setExploreShowDropDown] = useState(false);
-  const [ShowDropDown, SetShowDropDown] = useState(false);
+  import UserDropdropdown from "./UserDropdropdown";
+  const Navbar = () => {
+    const [ShowExploreDropdown, setExploreShowDropDown] = useState(false);
+    const [ShowDropDown, SetShowDropDown] = useState(false);
+    const [NotificationDropdown,setNotificationDropdown] = useState(false);
 
-  return (
-    <div className="w-full h-[11vh]  pl-[2vw] pr-[2vw] flex flex-row items-center justify-between text-[#FCFAFF] ">
-      <div className="navigation-area   relative flex items-baseline gap-[4vw] justify-center h-fit">
-        <span className="logo text-[1.7vw]">FlickStack.</span>
-        <Link to="/">Home</Link>
-        <Link
-          to="/Explore"
-          className="flex"
-          onMouseOver={() => setExploreShowDropDown(true)}
-          onMouseOut={() => setExploreShowDropDown(false)}
-        >
-          Explore
-          <motion.span
-            animate={{ rotate: ShowExploreDropdown ? 180 : 0 }}
-            transition={{ duration: 0.3 }}
-          >
-            <DropdownIcon size="text-[2vw]"></DropdownIcon>
-          </motion.span>
-          <ExploreDropdown
-            animateState={ShowExploreDropdown}
-            data={[
-              {
-                title: "Discover Photos",
-                icon: (
-                  <DiscoverIcon
-                    size="text-[1.7vw]"
-                    color="text-[#D8C1FF]"
-                  ></DiscoverIcon>
-                ),
-              },
-              {
-                title: "Trending Now",
-                icon: (
-                  <TrendingIcon
-                    size="text-[1.7vw]"
-                    color="text-[#D8C1FF]"
-                  ></TrendingIcon>
-                ),
-              },
-              {
-                title: "Most Liked",
-                icon: (
-                  <LikeIcon
-                    size="text-[1.7vw]"
-                    color="text-[#D8C1FF]"
-                  ></LikeIcon>
-                ),
-              },
-              {
-                title: "Based on Your Likes",
-                icon: (
-                  <UserPrefrenceIcon
-                    size="text-[1.7vw]"
-                    color="text-[#D8C1FF]"
-                  ></UserPrefrenceIcon>
-                ),
-              },
-            ]}
-          ></ExploreDropdown>
-        </Link>
-      </div>
-      <div className="search-box h-fit mr-[12vw]">
-        <span className="search-btn cursor-pointer">
-          <SearchIcon size="text-[1.8vw]"></SearchIcon>
-        </span>
-      </div>
-
-      <div className="user-navigation h-fit flex relative gap-[2vw] items-center justify-center">
-        <ChatIcon size="text-[1.8vw]"></ChatIcon>
-        <div className="relative">
-  <BellIcon size="text-[1.8vw] cursor-pointer">
-        </BellIcon>
-        <NotificationDropdropdown></NotificationDropdropdown>
-        </div>
+    return (
+      <>
       
-        <div
-          className="relative"
-          onMouseOver={() => SetShowDropDown(true)}
-          onMouseOut={() => SetShowDropDown(false)}
-        >
-           <div  className={`absolute -inset-[5px] rounded-[1vw] blur-xl opacity-50 z-0
+      
     
-    ${ShowDropDown ? "bg-gradient-to-r from-purple-500 to-pink-500 transition-all duration-600 ease-in-out" : ""}
-  `}></div>
-          <div className="w-[3.4vw] h-[3.4vw] cursor-pointer overflow-hidden relative rounded-[1vw] user-profile">
-            <img
-              src="https://avatars.githubusercontent.com/u/113233807?v=4"
-              className="w-full h-full object-cover"
-              alt=""
-            />
+      <div className="w-full h-[11vh] relative   pl-[2vw] pr-[2vw] flex flex-row items-center justify-between text-[#FCFAFF] ">
+        <div className="navigation-area   relative flex items-baseline gap-[4vw] justify-center h-fit">
+          <span className="logo text-[1.7vw]">FlickStack.</span>
+          <Link to="/">Home</Link>
+          <Link
+            to="/Explore"
+            className={`flex items-center justify-center ${ShowExploreDropdown ? "bg-[#1E1C21]" : ""} rounded-[1.6vw] pl-[1vw] p-[.5vw]`}
+            onMouseOver={() => setExploreShowDropDown(true)}
+            onMouseOut={() => setExploreShowDropDown(false)}
+          >
+            Explore
+            <motion.span className=" w-fit h-fit p-0"
+              animate={{ rotate: ShowExploreDropdown ? 180 : 0 }}
+              transition={{ duration: 0.3 }}
+            >
+              <DropdownIcon size="text-[2vw]"></DropdownIcon>
+            </motion.span>
+            <ExploreDropdown
+              animateState={ShowExploreDropdown}
+              data={[
+                {
+                  title: "Discover Photos",
+                  icon: (
+                    <DiscoverIcon
+                      size="text-[1.7vw]"
+                      color="text-[#D8C1FF]"
+                    ></DiscoverIcon>
+                  ),
+                },
+                {
+                  title: "Trending Now",
+                  icon: (
+                    <TrendingIcon
+                      size="text-[1.7vw]"
+                      color="text-[#D8C1FF]"
+                    ></TrendingIcon>
+                  ),
+                },
+                {
+                  title: "Most Liked",
+                  icon: (
+                    <LikeIcon
+                      size="text-[1.7vw]"
+                      color="text-[#D8C1FF]"
+                    ></LikeIcon>
+                  ),
+                },
+                {
+                  title: "Based on Your Likes",
+                  icon: (
+                    <UserPrefrenceIcon
+                      size="text-[1.7vw]"
+                      color="text-[#D8C1FF]"
+                    ></UserPrefrenceIcon>
+                  ),
+                },
+              ]}
+            ></ExploreDropdown>
+          </Link>
+        </div>
+        <div className="search-box   relative h-fit cursor-pointer transition-all duration-450 ease-in-out
+ hover:bg-[#1E1C21]  p-[.8vw] rounded-full mr-[12vw]">
+             <div  className="absolute -inset-[5px] rounded-[1vw] blur-xl opacity-50 z-0
+      
+      hover:bg-gradient-to-r from-purple-500 to-pink-500 transition-all duration-600 ease-in-out 
+    "></div>
+          <span className="search-btn cursor-pointer"> 
+            <SearchIcon size="text-[1.8vw]"></SearchIcon>
+          </span>
+         
+        </div>
+
+        <div className="user-navigation h-fit flex relative gap-[2vw] items-center justify-center">
+          <ChatIcon size="text-[1.8vw]"></ChatIcon>
+          <div className={`relative cursor-pointer ${NotificationDropdown ? "bg-[#1E1C21] ":""} p-[.9vw] rounded-full`} onMouseOver={()=>setNotificationDropdown(true)} onMouseOut={()=>setNotificationDropdown(false)}>
+    <BellIcon size="text-[1.8vw] cursor-pointer">
+          </BellIcon>
+          <NotificationDropdropdown animateState={NotificationDropdown}></NotificationDropdropdown>
           </div>
-          <UserDropdropdown
+        
+          <div
+            className="relative"
             onMouseOver={() => SetShowDropDown(true)}
-            animateState={ShowDropDown}
-          ></UserDropdropdown>
+            onMouseOut={() => SetShowDropDown(false)}
+          >
+            <div  className={`absolute -inset-[5px] rounded-[1vw] blur-xl opacity-50 z-0
+      
+      ${ShowDropDown ? "bg-gradient-to-r from-purple-500 to-pink-500 transition-all duration-600 ease-in-out" : ""}
+    `}></div>
+            <div className="w-[3.4vw] h-[3.4vw] cursor-pointer overflow-hidden relative rounded-[1vw] user-profile">
+              <img
+                src="https://avatars.githubusercontent.com/u/113233807?v=4"
+                className="w-full h-full object-cover"
+                alt=""
+              />
+            </div>
+            <UserDropdropdown
+              onMouseOver={() => SetShowDropDown(true)}
+              animateState={ShowDropDown}
+            ></UserDropdropdown>
+          </div>
         </div>
       </div>
-    </div>
-  );
-};
+               
+      </>
+    );
+  };
 
-export default Navbar;
+  export default Navbar;
--- src/components/NotificationDropdropdown.jsx
+++ src/components/NotificationDropdropdown.jsx
@@ -1,15 +1,28 @@
 import React from 'react'
 import AlertIcon from '../icons/AlertIcon'
-const NotificationDropdropdown = () => {
+import { motion, AnimatePresence } from 'framer-motion'
+
+const NotificationDropdropdown = ({ animateState }) => {
   return (
-    <>
-     <div className="notification-panel absolute top-[3.5vw] right-0 bg-[#1E1C21] p-[.8vw] rounded-[1vw] flex flex-col gap-[.8vw] w-[20vw] h-[25vw] ">
-           <h2 className="headline h-fit border-2 p-[.9vw] flex items-center justify-between">
-            Notification <span><AlertIcon size="text-[1.7vw]" color="text-[#D8C1FF]"></AlertIcon></span>
-           </h2>
-         </div>
-    
-    </>
+    <AnimatePresence>
+      {animateState && (
+        <motion.div
+          initial={{ opacity: 0, y: -20 }}
+          animate={{ opacity: 1, y: 0 }}
+          exit={{ opacity: 0, y: -20}}
+          transition={{ duration: 0.3, ease: 'easeInOut' }}
+          className="notification-panel absolute top-[4.5vw] right-0 bg-[#1E1C21] rounded-[1vw] flex flex-col gap-[.8vw] w-[25vw] h-[30vw] z-50"
+        >
+          <h2 className="headline h-fit pb-[.8vw] pl-[.8vw] pr-[.8vw] pt-[.8vw] text-[1.11vw] relative flex items-center justify-between">
+            Your Notifications
+            <span>
+              <AlertIcon size="text-[1.7vw]" color="text-[#D8C1FF]" />
+            </span>
+            <span className="underline absolute bottom-0 left-0 w-full h-[.1vh] bg-[#d8c1ff31]"></span>
+          </h2>
+        </motion.div>
+      )}
+    </AnimatePresence>
   )
 }
 
