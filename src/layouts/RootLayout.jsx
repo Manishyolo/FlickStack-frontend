52b8a6b7174d61d0c8eee960f34af531c3a5b80b
Manish kumar <manish04kumarsingh@gmail.com>
2025-06-27 09:09:28 +0530

    feat: add nprogress for loading indicators; update RootLayout to manage progress state; include nprogress styles

--- src/layouts/RootLayout.jsx
+++ src/layouts/RootLayout.jsx
@@ -1,9 +1,32 @@
 import React from 'react'
 import Navbar from '../components/Navbar'
 import { Outlet } from 'react-router'
+import { useLocation } from 'react-router'
+import { useEffect } from 'react'
+import NProgress from 'nprogress'
 
 
 const RootLayout = () => {
+ const location = useLocation();
+
+  useEffect(()=>{
+     NProgress.start();
+     
+     const timeOut = setTimeout(()=>{
+   NProgress.done();
+
+     },500)
+
+ return ()=>{
+    clearTimeout(timeOut);
+    NProgress.done();
+ }
+
+
+  },[location]);
+
+
+
   return (
     <>
     <Navbar></Navbar>
--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,10 @@ import { createRoot } from 'react-dom/client'
 import router from './router/router.jsx';
 import { RouterProvider } from 'react-router';
 import "./main.css"
+import 'nprogress/nprogress.css';
+import "./css/nprogress.css";
 
-import App from './App.jsx'
+
 
 createRoot(document.getElementById('root')).render(
   
