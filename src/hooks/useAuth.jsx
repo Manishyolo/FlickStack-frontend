adcc20ecf11f86eb02e412f8760a949e46e40c8b
Manish kumar <manish04kumarsingh@gmail.com>
2026-02-14 17:30:40 +0530

    feat: add useAuth hook for authentication state management; initialize isAuthenticated and loading states

---
+++ src/hooks/useAuth.jsx
@@ -0,0 +1,15 @@
+import { useState,useEffect } from "react";
+import axios from "axios";
+
+
+export const useAuth = ()=>{
+    const [isAuthenticated,setisAuthenticated] = useState(false);
+    const [loading,setLoading] = useState(true);
+   
+    
+
+
+
+
+
+}
